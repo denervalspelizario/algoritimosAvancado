59f1d700853a861e0437a43a12b02644d0159651
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-09-12 09:25:27 -0300

    estudo algoritimos resolucao simple array sum

---
+++ 01-SIMPLE-ARRAY-SUM/SIMPLE-ARRAY-SUM.js
@@ -0,0 +1,34 @@
+/*
+
+SOMA SIMPLES DE ARRAY
+
+
+Declaração do problema
+Dado um conjunto de inteiros, encontre a soma dos seus elementos.
+
+Por exemplo, se a matriz $ar = [1,2,3]1+2+3 =6, então retorne 6.
+
+Descrição da função
+Complete a função simpleArraySum no editor abaixo. Ela deve retornar a soma dos elementos do array como um inteiro.
+
+simpleArraySum tem o(s) seguinte(s) parâmetro(s):
+
+ar: uma matriz de inteiros
+
+Sample Input : 1 2 3 4 10 11
+Sample Output : 31
+*/
+
+// Resposta pessoal 
+let sum = 0;
+let arrayQueSeraSomado = [1,2,3,4,10,11]
+
+
+for(let i = 0; i < arrayQueSeraSomado.length; i++)
+{
+  sum = sum + arrayQueSeraSomado[i]
+}
+
+console.log(sum)
+
+// Resposta instrutor
